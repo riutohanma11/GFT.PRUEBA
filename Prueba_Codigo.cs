a620386c687cb279f3887246907e0d3ba5763eaf
Pena Villamizar <jnvm@gft.com>
2024-08-09 12:52:31 -0500

    prueba DLP GFT

---
+++ Prueba_Codigo.cs
@@ -0,0 +1,27 @@
+using System;
+ 
+class Program
+{
+    static void Main()
+    {
+        // Solicitar al usuario el primer número
+        Console.Write("Ingrese el primer número: ");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+ 
+        // Solicitar al usuario el segundo número
+        Console.Write("Ingrese el segundo número: ");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+ 
+        // Llamar a la función de suma
+        double resultado = Suma(numero1, numero2);
+ 
+        // Mostrar el resultado
+        Console.WriteLine($"La suma de {numero1} y {numero2} es: {resultado}");
+    }
+ 
+    // Función para sumar dos números
+    static double Suma(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+}
